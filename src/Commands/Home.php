13c3c54e316e903ddeca4abb9386dd5893a1765c
Al Saleh <alayham@alayham.com>
2020-11-12 21:07:32 +0100

    The home page

---
+++ src/Commands/Home.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\gradient_server\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\layout_builder\Plugin\SectionStorage\OverridesSectionStorage;
+use Drupal\node\Entity\NodeType;
+use Drupal\node\Entity\Node;
+use Drupal\layout_builder\Section;
+use Drupal\layout_builder\SectionComponent;
+
+class Home extends DrushCommands {
+
+  /**
+   * Create a homepage.
+   *
+   * @command gradient:homepage:create
+   * @aliases ghc
+   * @usage gradient:homepage:create
+   */
+  public function create() {
+    $section1 = new Section('layout_twocol_section', ['column_widths' => '75-25']);
+    $section1->appendComponent(new SectionComponent(\Drupal::service('uuid')->generate(), 'first', [
+      'id' => 'views_block:new_items-block_1',
+      'label' => 'Test block title',
+      'label_display' => 'hidden',
+    ]));
+    $section1->appendComponent(new SectionComponent(\Drupal::service('uuid')->generate(), 'second', [
+      'id' => 'views_block:new_feeds-block_1',
+      'label' => 'Test block title',
+      'label_display' => 'hidden',
+    ]));
+    $section1->appendComponent(new SectionComponent(\Drupal::service('uuid')->generate(), 'second', [
+      'id' => 'views_block:new_sources-block_1',
+      'label' => 'Test block title',
+      'label_display' => 'hidden',
+    ]));
+    $section1->appendComponent(new SectionComponent(\Drupal::service('uuid')->generate(), 'second', [
+      'id' => 'views_block:new_gradients-block_1',
+      'label' => 'Test block title',
+      'label_display' => 'hidden',
+    ]));
+    $page = Node::create([
+      'type' => 'landing_page',
+      'title' => 'Welcome to Gradient Server',
+      'status' => 1,
+      OverridesSectionStorage::FIELD_NAME => [$section1],
+    ]);
+    $page->save();
+    \Drupal::configFactory()
+      ->getEditable('system.site')
+      ->set('page.front', $page->toUrl()->toString())
+      ->save();
+  }
+
+}
